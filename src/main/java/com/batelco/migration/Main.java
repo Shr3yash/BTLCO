e919b098565af830b924b72e1bd7cad70c1b5da2
501414 <501414@btc.com.bh>
2025-04-10 10:49:44 +0530

    Updated CA Account

--- src/main/java/com/batelco/migration/Main.java
+++ src/main/java/com/batelco/migration/Main.java
@@ -12,7 +12,7 @@ public class Main {
         String sqlQuery = "SELECT * FROM stg_cust_acct";
         
         // Output XML file path
-        String outputFile = "outputBillingInfo.xml";
+        String outputFile = "customer.xml";
         
         // Generate XML
         // XMLGenerator.generateXML(sqlQuery, outputFile);
