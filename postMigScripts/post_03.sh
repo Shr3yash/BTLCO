bdfdff7f5235817b87846781c2b56ff02ebb5b79
501414 <501414@btc.com.bh>
2025-11-16 20:30:30 +0530

    UPDATE POST_03 POST MIG SCRIPT

--- postMigScripts/post_03.sh
+++ postMigScripts/post_03.sh
@@ -2,9 +2,10 @@
 set -euo pipefail
 source ./common.sh
 run_sql "post" "03" "
-exec post_mig_billing_account_payinfo_data;
-exec post_mig_billing_account_bilinfo_data;
-exec post_mig_billing_account_bill_data;
-exec post_mig_billing_account_invoice_data;
-exec post_mig_billing_account_status_data;
+exec    post_mig_billing_account_payinfo_data;
+exec    post_mig_billing_account_billinfo_data;
+exec    post_mig_billing_account_bill_data;
+exec    post_mig_billing_account_bal_grp_data; -- newly added
+exec    post_mig_billing_account_invoice_data;
+exec    post_mig_billing_account_status_data;
 "
